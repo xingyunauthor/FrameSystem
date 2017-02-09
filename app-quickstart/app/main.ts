6a12c47b9c8ab91fb0654de76afd29920ef1c68f
apple <apple@APPLE1794>
2017-02-09 09:04:16 +0800

    新增

---
+++ app-quickstart/app/main.ts
@@ -0,0 +1,4 @@
+﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
