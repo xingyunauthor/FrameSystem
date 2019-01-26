473cf60a165d7ac25417203ec2363affc78906de
韩春先 <hanchunxian@os.vcredit.com>
2019-01-26 21:52:28 +0800

    更新缺少的两个文件

---
+++ Frame.Models/SysModels/Log/LogAddRequestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Frame.Models.SysModels.Log
+{
+    public class LogAddRequestModel
+    {
+        public string LoginName { get; set; }
+
+        public DateTime LoginTime { get; set; }
+
+        public string LoginRole { get; set; }
+
+        public string LoginMach { get; set; }
+
+        public string LoginCpu { get; set; }
+    }
+}
---
+++ Frame.Models/SysModels/Log/LogAllResponseModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Frame.Models.SysModels.Log
+{
+    public class LogAllResponseModel
+    {
+        public int RowId { get; set; }
+
+        public int LogId { get; set; }
+
+        public string LoginName { get; set; }
+
+        public string LoginTime { get; set; }
+
+        public string LoginRole { get; set; }
+
+        public string LoginMach { get; set; }
+
+        public string LoginCpu { get; set; }
+    }
+}
